0d0372360df5c686210009174fc2b932fc6db116
whillas-yabble <alex@yabblezone.net>
2022-01-05 11:51:54 +1300

    baddie bouncing

--- pong/spacenumbers.py
+++ pong/spacenumbers.py
@@ -1,14 +1,35 @@
+from random import randint
+
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.properties import NumericProperty, ObjectProperty, ReferenceListProperty
 from kivy.uix.widget import Widget
+from kivy.vector import Vector
+
+
+class NaughtyBaddie(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        if (self.y < 0) or (self.top > self.parent.height):
+            self.velocity_y *= -1
+
+        if (self.x < 0) or (self.right > self.parent.width):
+            self.velocity_x *= -1
+
+        self.pos = Vector(*self.velocity) + self.pos
 
 
 class SpaceNumbersGame(Widget):
+    baddie = ObjectProperty(None)
+
     def start(self):
-        ...
+        self.baddie.velocity = Vector(4, 0).rotate(randint(0, 360))
 
     def update(self, dt):
-        ...
+        self.baddie.move()
 
 
 class SpaceNumbersApp(App):
